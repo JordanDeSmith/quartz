ae59b6616d88810c97de60ca3755cae83418e69e
JordanSmith <great.jordini@gmail.com>
2021-08-05 10:01:47 -0600

    More efficient accessing of data through dictionary

--- keyboard.py
+++ keyboard.py
@@ -1,5 +1,3 @@
-from kivy import config
-from kivy.uix.behaviors import button
 from soundPlayer import SoundPlayer
 import os
 import json
@@ -27,7 +25,13 @@ class Keyboard(Widget):
         for file in os.listdir():
             if file.endswith(".json"):
                 self.jsonFiles.append(file)
-        self.configData = json.load(open(self.jsonFiles[0]))
+        jsonData = json.load(open(self.jsonFiles[0]))
+        self.configData = {}
+        for i in jsonData:
+            if i["key"] in self.configData:
+                self.configData[i["key"]].append({"modifiers":i["modifiers"],"type":i["type"],"data":i["data"]})
+            else:
+                self.configData[i["key"]] = [{"modifiers":i["modifiers"],"type":i["type"],"data":i["data"]}]
         self.currentlyLooping = []
 
         dropDown = DropDown()
@@ -53,25 +57,26 @@ class Keyboard(Widget):
         MODIFIERS = ['shift','rshift','alt','alt-gr','lctrl','rctrl']
         if (keycode[1] not in MODIFIERS):
             try:
-                for i in self.configData:
-                    if (i["key"] == keycode[1] and set(i["modifiers"]) == set(modifiers)):
-                        if i["type"] == "sound":
-                            if (i["loopable"]):
-                                if ((keycode[1], modifiers) in self.currentlyLooping):
-                                    self.soundPlayer.stopRepeating(i["filePath"])
-                                    self.currentlyLooping.remove((keycode[1], modifiers))
+                if keycode[1] in self.configData:
+                    for i in self.configData[keycode[1]]:
+                        if (set(i["modifiers"]) == set(modifiers)):
+                            if i["type"] == "sound":
+                                if (i["data"]["loopable"]):
+                                    if ((keycode[1], modifiers) in self.currentlyLooping):
+                                        self.soundPlayer.stopRepeating(i["data"]["filePath"])
+                                        self.currentlyLooping.remove((keycode[1], modifiers))
+                                    else:
+                                        self.soundPlayer.playSound(i["data"]["filePath"], True)
+                                        self.currentlyLooping.append((keycode[1], modifiers))
                                 else:
-                                    self.soundPlayer.playSound(i["filePath"], True)
-                                    self.currentlyLooping.append((keycode[1], modifiers))
-                            else:
-                                self.soundPlayer.playSound(i["filePath"])
-                        elif i["type"] == "stopAll":
-                            self.soundPlayer.stopAll()
-                            self.currentlyLooping = []
-                        elif i["type"] == "stopLooping":
-                            self.soundPlayer.stopAllRepeating()
-                            self. currentlyLooping = []
-                        break;
+                                    self.soundPlayer.playSound(i["data"]["filePath"])
+                            elif i["type"] == "stopAll":
+                                self.soundPlayer.stopAll()
+                                self.currentlyLooping = []
+                            elif i["type"] == "stopLooping":
+                                self.soundPlayer.stopAllRepeating()
+                                self. currentlyLooping = []
+                            break;
             except FileNotFoundError as e:
                 print(e)
             except ValueError as e:
@@ -80,7 +85,13 @@ class Keyboard(Widget):
         return True
 
     def change_config(self, configFile):
-        self.configData = json.load(open(configFile))
+        jsonData = json.load(open(configFile))
+        self.configData = {}
+        for i in jsonData:
+            if i["key"] in self.configData:
+                self.configData[i["key"]].append({"modifiers":i["modifiers"],"type":i["type"],"data":i["data"]})
+            else:
+                self.configData[i["key"]] = [{"modifiers":i["modifiers"],"type":i["type"],"data":i["data"]}]
         setattr(self.configButton, "text", configFile)
 
 
