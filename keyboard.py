c3a938260c24eb052a364e1319279a854cd75b0a
JordanSmith <great.jordini@gmail.com>
2022-01-28 13:10:33 -0700

    basic setup for modifying and saving files

--- keyboard.py
+++ keyboard.py
@@ -6,6 +6,9 @@ import json
 import kivy
 from kivy.uix.widget import Widget
 from kivy.app import App
+from kivy.uix.popup import Popup
+from kivy.uix.dropdown import DropDown
+from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
@@ -15,6 +18,7 @@ from soundPlayer import SoundPlayer
 kivy.require('2.0.0')
 
 MODIFIERS = ['shift','rshift','alt','ctrl','capslock','meta']
+ACTION_TYPES = ['sound', 'stopAll', 'stopLooping']
 
 class Observer():
     """Allows for observers to listen to different events
@@ -96,24 +100,103 @@ class Keyboard(Widget, Observer):
 
         return True
 
+class LoadFile(BoxLayout):
+    """Loads a sound file"""
+    def __init__(self, load_callback, **kwargs):
+        super(LoadFile, self).__init__(**kwargs)
+        self.load_callback = load_callback
+        self.file_chooser = FileChooserListView(filters=['*.wav'], path='./sounds/')
+        self.popup_layout = BoxLayout(orientation="vertical")
+        self.popup_layout.add_widget(self.file_chooser)
+        self._popup = Popup(title="Load file", content=self.popup_layout,
+                size_hint=(0.9, 0.9))
+        self.popup_layout.add_widget(Button(size_hint=(1,0.1), text="Cancel", on_release=self._popup.dismiss))
+        self.popup_layout.add_widget(Button(size_hint=(1,0.1), text="Load", 
+            on_release=lambda btn: self.load()))
+
+        self.add_widget(Button(text="Load file", size_hint=(1,1), 
+            on_release=lambda button: self._popup.open()))
+    
+    def load(self):
+        self._popup.dismiss()
+        self.load_callback(self.file_chooser.path, self.file_chooser.selection)
+
 class EditKeyboard(AnchorLayout, Observer):
     """Displays keyboard and allows for edits to config"""
     def __init__(self, config, config_modifier, layout_file, **kwargs):
         super(EditKeyboard, self).__init__(**kwargs)
         self.config = config
-        self.config_modifier = config_modifier
+        self.new_file = None
         self.edit_modifiers = set()
-        self.lit_button = None
         self.edit_key = None
+        self.edit_type = None
+        self.lit_button = None
         self.key_layout = None
+        self.edit_button = Button(size_hint=(1,1), text="Edit", 
+            on_release=lambda button: self.edit(True))
+        self.editing_box = BoxLayout(orientation="horizontal")
+        self.load_file = LoadFile(self.change_file)
+        self.drop_down = DropDown()
+        for action in ACTION_TYPES:
+            self.drop_down.add_widget(Button(height=50, size_hint_y=None, text=action, 
+                on_release=lambda btn: self.drop_down.select(btn.text)))
+        type_button = Button(size_hint=(0.6,1), text=ACTION_TYPES[0], on_release=self.drop_down.open)
+        self.drop_down.bind(on_select=lambda instance, x: self.change_action(type_button, x))
+        self.editing_box.add_widget(type_button)
+        self.editing_box.add_widget(self.load_file)
+        self.editing_box.add_widget(Button(text="Cancel", size_hint=(0.3,1),
+            on_release=lambda btn: self.edit(False)))
+        self.editing_box.add_widget(Button(size_hint=(0.3,1), text="Save",
+            on_release=lambda btn: self.save_change(config_modifier)))
+
+
         self.layout = BoxLayout(size_hint=(1,1), orientation="vertical")
         self.add_widget(self.layout)
-        self.label = Label(size_hint=(1,0.1), text="Select a key to see its config")
-        self.layout.add_widget(self.label)
+        
+        display_layout = BoxLayout(size_hint=(1,0.1), orientation="horizontal")
+        self.layout.add_widget(display_layout)
+        self.label = Label(size_hint=(1,1), text="Select a key to see its config")
+
+        self.edit_layout = BoxLayout(size_hint=(1,1), orientation="horizontal")
+        display_layout.add_widget(self.label)
+        display_layout.add_widget(self.edit_layout)
+        self.edit(False)
         try:
             self.change_layout(layout_file)
         except ValueError as error:
             print(error) #TODO: Better handle this error
+
+    def save_change(self, config_modifier):
+        self.edit(False)
+        #TODO: Double check values
+        config_modifier(self.edit_key, self.edit_modifiers, self.edit_type,
+            self.new_file, loopable=False) #TODO: Get input on whether sound should loop or not
+
+    def change_file(self, path, file):
+        #TODO: Display the loaded file
+        if len(file) > 0:
+            self.new_file = os.path.join(path, file[0])
+        else:
+            self.new_file = None
+
+    def change_action(self, button, action):
+        setattr(button, 'text', action)
+        self.edit_type = action
+        if action == "sound":
+            self.load_file.disabled = False
+        else:
+            self.load_file.disabled = True
+
+    def edit(self, open):
+        self.edit_layout.clear_widgets()
+        if open:
+            self.edit_layout.size_hint=(1,1)
+            self.edit_layout.add_widget(self.editing_box)
+            self.drop_down.select(ACTION_TYPES[0])    #TODO: load what the current key type is if valid
+        else:
+            self.edit_layout.size_hint=(0.1,1)
+            self.edit_layout.add_widget(self.edit_button)
+
 
     def change_layout(self, file_name):
         """Changes the edit_keyboard layout"""
@@ -140,8 +223,8 @@ class EditKeyboard(AnchorLayout, Observer):
             raise ValueError("Invalid option in layout file")
 
     def update_config(self, config):
-        #TODO: check if something has been changed and needs to be saved
         self.config = config
+        #TODO: Find a better way to make sure this updates
         if self.lit_button is not None:
             self.lit_button.background_color = [1,1,1,1]
             self.lit_button = None
@@ -178,7 +261,34 @@ class EditKeyboard(AnchorLayout, Observer):
                     break;
         if not_in_config:
             self.label.text = "No config"
-        
+
+class SaveChanges(Widget):
+    def __init__(self, save_callback, data, file, **kwargs):
+        self.exit = False
+        if "exit" in kwargs:
+            self.exit = kwargs.get("exit")
+            kwargs.pop("exit")            #Kivy's Widget doesn't like any kwargs it's not expecting
+        super(SaveChanges, self).__init__(**kwargs)
+        self.callback = save_callback
+        self.data = data
+        self.file = file
+        popup_layout = BoxLayout()
+        self.save_popup = Popup(title="Unsaved Changes", content=popup_layout, size_hint=(1,0.2))
+        popup_layout.add_widget(Label(text="You have unsaved changes"))
+        popup_layout.add_widget(Button(text="Discard", 
+            on_release=lambda btn: self.close(False)))
+        popup_layout.add_widget(Button(text="Save",
+            on_release=lambda btn: self.close(True)))
+
+        self.save_popup.open()
+
+    def close(self, save):
+        self.save_popup.dismiss()
+        if save:
+            self.callback(self.data, self.file)
+        if self.exit:
+            App.get_running_app().stop()
+
 
 class KeyboardApp(App):
     """Application start for Kivy"""
@@ -227,6 +337,9 @@ class KeyboardApp(App):
         if config is self.config:
             token = (section,key)
             if token == ('keyboard', 'last_used_config'):
+                if self.changed_config:
+                    SaveChanges(self.save_changes, self.config_data, self.config_file) #FIXME: Pulls filename
+                self.config_file = value
                 self.update_config_data(value)
                 self.config_button.text = value
             if token == ('keyboard', 'layout_config'):
@@ -245,12 +358,15 @@ class KeyboardApp(App):
                 self.config_data[i["key"]] = [
                     {"modifiers":i["modifiers"],"type":i["type"],"data":i["data"]}]
         Observer.notify("config_update", self.config_data)
+        self.changed_config = False
 
     def build(self):
         parent = BoxLayout(orientation='vertical')
         Window.bind(on_request_close=self.on_request_close)
         Window.size = (750,500)
         self.settings = self.config
+        self.changed_config = False
+        self.config_file = ""
 
         self.json_files = []
         for file in os.listdir(os.path.join(os.path.curdir, self.CONFIG_PATH)):
@@ -259,15 +375,14 @@ class KeyboardApp(App):
         if len(self.json_files) == 0:
             raise RuntimeError("No config files") #TODO: Properly handle there being no config files.
 
-        config_file = ""
         if self.settings.get('keyboard', 'last_used_config') != "None" and self.settings.get(
                 'keyboard', 'last_used_config') in self.json_files:
-            config_file = self.settings.get('keyboard', 'last_used_config')
+            self.config_file = self.settings.get('keyboard', 'last_used_config')
         else:
             self.config.set('keyboard', 'last_used_config', self.json_files[0])
             self.config.write()
-            config_file = self.json_files[0]
-        self.update_config_data(config_file)
+            self.config_file = self.json_files[0]
+        self.update_config_data(self.config_file)
 
         self.layout_files = []
         for file in os.listdir(os.path.join(os.path.curdir, self.LAYOUT_PATH)):
@@ -302,11 +417,47 @@ class KeyboardApp(App):
         return parent
 
     def on_request_close(self, *args):
-        pass #TODO: write out anything we need/check if things have changed
+        if self.changed_config:
+            SaveChanges(self.save_changes, self.config_data, self.config_file, exit=True)
+            return True
+        else:
+            return False
 
-    def edit_config(self, key, modifiers, new_file):
-        print(f"Editing config\nKey: {key}\nModifiers: {modifiers}")
-        Observer.notify("config_update", self.config_data)
+    def save_changes(self, config, file):
+        print(f"Saving changes to: {file}")
+        self.changed_config = False
+
+    def edit_config(self, key, modifiers, new_type, new_file=None, loopable=False):
+        #TODO: Fix this function, it's super redundant
+        modifiers = list(modifiers)
+        if key in self.config_data:
+            entered = False
+            for item in self.config_data.get(key):
+                if set(modifiers) == set(item["modifiers"]):
+                    entered = True
+                    item["type"] = new_type
+                    if new_type == "sound":
+                        item["data"]["filePath"] = new_file
+                        item["data"]["loopable"] = loopable
+                    else:
+                        item["data"] = {}
+            if not entered:
+                new_data = {"modifiers":modifiers, "type":new_type, "data":{}}
+                if new_type == "sound":
+                    new_data["data"]["filePath"] = new_file
+                    new_data["data"]["loopable"] = loopable
+                self.config_data[key].append(new_data)
+                self.changed_config = True
+        else:
+            new_data = {"modifiers":modifiers, "type":new_type, "data":{}}
+            if new_type == "sound":
+                    new_data["data"]["filePath"] = new_file
+                    new_data["data"]["loopable"] = loopable
+            self.config_data[key] = [new_data]
+            self.changed_config = True
+
+        if self.changed_config:
+            Observer.notify("config_update", self.config_data)
 
     def reset_settings(self):
         """Resets all settings to default values"""
